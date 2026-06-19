31502def50910f84ebf841b02b034118d3ed3aa9
Amit <amitdewangan019@gmail.com>
2026-06-19 08:09:50 +0530

    Added store and components

---
+++ src/appwrite/config.js
@@ -0,0 +1,129 @@
+import conf from "../conf/conf";
+
+import { Client, ID, Databases, Storage, Query, TablesDB } from "appwrite";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.databases = new TablesDB(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createRow({
+        databaseId: conf.appwriteDatabaseId,
+        tableId: conf.appwriteCollectionId,
+        rowId: slug,
+        data: {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        },
+      });
+    } catch (error) {
+      console.log("Appwrite service :: creatPost :: error", error);
+    }
+  }
+
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return this.databases.updateRow(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        },
+      );
+    } catch (error) {
+      console.log("Appwrite service :: updatePost:: error", error);
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      await this.databases.deleteRow(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+      );
+      return true;
+    } catch (error) {
+      console.log("Appwrite service :: deletePost :: error");
+      return false;
+    }
+  }
+
+  async getPost(slug) {
+    try {
+      return await this.databases.getRow(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+      );
+    } catch (error) {
+      console.log("Appwrite service :: getPost :: error", error);
+    }
+  }
+
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listRows(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries,
+      );
+    } catch (error) {
+      console.log("Appwrite service :: getPosts :: error", error);
+    }
+  }
+
+  // file upload service
+
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile({
+        bucketId: conf.appwriteBucketId,
+        fileId: ID.unique(),
+        file,
+      });
+    } catch (error) {
+      console.log("Appwrite service :: uploadFile :: error", error);
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      return await this.bucket.deleteFile({
+        bucketId: conf.appwriteBucketId,
+        fileId: ID.unique(),
+      });
+    } catch (error) {
+      console.log("Appwrite service :: deleteFile :: error", error);
+      return false;
+    }
+  }
+
+  getFilePreview(fileId) {
+    return this.bucket.getFilePreview({
+      bucketId: conf.appwriteCollectionId,
+      fileId,
+    });
+  }
+}
+
+const service = new Service();
+
+export default service;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Footer from "./Footer/footer";
+import Header from "./Header/Header";
+
+export { Header, Footer };
