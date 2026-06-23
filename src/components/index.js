0a7f7ba2b31023572e62ce8b0a69be08ffc9ee3f
Amit <amitdewangan019@gmail.com>
2026-06-23 11:31:09 +0530

    Add pages

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,27 @@
 import Footer from "./Footer/footer";
 import Header from "./Header/Header";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./input";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
 
-export { Header, Footer };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  Input,
+  RTE,
+  Signup,
+  Login,
+  PostForm,
+  PostCard,
+};
