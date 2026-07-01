f96bda1fe06ac423d636ff2a43567f6946b560d8
Amit <amitdewangan019@gmail.com>
2026-07-01 15:33:38 +0530

    add logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,12 +3,11 @@ import React from "react";
 function Logo({ width = "100px" }) {
   return (
     <div>
-      <img
-        height="30px"
-        width="30px"
-        src="https://i.pinimg.com/1200x/af/2c/27/af2c2758a723f18ee6bcb06247f7f45c.jpg"
-        alt=""
-      />
+      <div className="bg-gray-900 h-[50px] w-[50px] rounded-full flex justify-center items-center">
+        <span className="text-white pl-1 font-bold text-2xl tracking-tighter">
+          A <sup>2</sup>
+        </span>
+      </div>
     </div>
   );
 }
